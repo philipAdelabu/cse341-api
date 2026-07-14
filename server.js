af12eb97214cbb9a2ddd87a051ea74dd75479b3b
Philip Adelabu <adebisiphilip@gmail.com>
2026-07-14 09:42:57 +0100

    fixed the response header for cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, any_key, Authorization');
    res.setHeader('Access-Control-Allow-Credentials', '*');
   next();
 });
